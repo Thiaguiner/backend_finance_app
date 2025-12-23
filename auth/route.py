3d8e8d8e6ccc20df876b59ec0999c3f656d996b5
Thiaguiner <tcarneiro990@gmail.com>
2025-12-23 16:02:55 -0300

    route blueprint

--- auth/route.py
+++ auth/route.py
@@ -1,14 +1,18 @@
-from flask import request
-from server import app
+from flask import request, Flask, Blueprint
 from .controller import AuthController
 
 
-@app.route('/login', methods=['POST'])
+auth_bp = Blueprint('auth', __name__, template_folder='templates')
+@auth_bp.post('/login')
 def login():
     data = request.get_json()
     print('DATA RECEBIDA:', data)
 
-    return AuthController().loggin_controller(
-        data.get('login'),
-        data.get('senha')
-    )
+    return AuthController().loggin_controller(data.get('login'),data.get('senha'))
+
+@auth_bp.post('/cadastro')
+def cadastro():
+    data = request.get_json()
+    print('DATA RECEBIDA:', data)
+
+    return AuthController().cadastro_controller(data.get('nome'),data.get('senha'), data.get('email'))
--- server.py
+++ server.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_cors import CORS
+from auth.route import auth_bp
 
 app = Flask(__name__)
-CORS(app)
 
-# IMPORT DAS ROTAS DEPOIS DO APP EXISTIR
-import auth.route
+CORS(app, supports_credentials=True)
+
+
+app.register_blueprint(auth_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
